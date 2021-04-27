e64ecd4ecb4366a314eb1a777859eae71342ca23
sampada1204 <67230655+sampada1204@users.noreply.github.com>
2021-04-27 12:13:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+print("enter a string to encrypt")
+A=["A","B" , "C" , "D", "E" , "F", "G", "H", "I", "J, K, L, M, N, O, P, Q, R, S, T,U, V,W, X, Y,Z,"]
+B=["I, J, K, L, M, N, O, P, Q, R, S, T, U, V," "W, X, Y, Z, A,B, C, D, E, F, G, H"]
+FINALWORD=""
+STRING = INPUT("")
+while STRING:
+   if STRING!=chr:
+      FINALWORD=FINALWORD+STRING
+      else
+      STRING=STRING.UPPER()
+      STRING= B.INDEX(A.INDEX())
+      FINALWORD+=STRING
+
+      [x.upper() for x in["a","b","c"]]
+      ["A","B","C"]
+
+
